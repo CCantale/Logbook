84f3f404d030c315cdd3f959c8964a2eb9557066
ccantale <claudio.cantale93@gmail.com>
2023-07-17 06:28:55 +0200

    16/07/2023
    First commit. Nothing special to say. Have a nice one :)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Logbook
-Easy C++ logging system
+Just a little logging system to avoid a too crowded terminal while your program prints out every damn thing it does. Every. Damn. Thing.
+
+Doc is in the .h file. Peace!
---
+++ log.h
@@ -0,0 +1,82 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                                            */
+/*   log.h                                                 by CanTale_Games   */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*   First call Log::init(), then use Log::lout, Log::lwar and Log::lerr      */
+/*   instead of std::cout, std::cwar and std::cerr.                           */
+/*   The macro "timestamp" can be inserted in the stream to have a nice       */
+/*   timestamp in the entry.                                                  */
+/*   Remember to call Log::quit() at the end of your program.                 */
+/*   Logbook will create a new directory named "Logbook" with a log file      */
+/*   inside. cat it to see the log in colors. The file is overwritten         */
+/*   everytime the program runs.                                              */
+/*                                                                            */
+/*                                                                            */
+/*   Example:                                                                 */
+/*                                                                            */
+/*   Log::init();                                                             */
+/*                                                                            */
+/*   ...                                                                      */
+/*                                                                            */
+/*   if (errcode == 0)                                                        */
+/*   {                                                                        */
+/*       Log::lout << timestamp << "Everything up and running!\n";            */
+/*   }                                                                        */
+/*   else                                                                     */
+/*   {                                                                        */
+/*       Log::lerr << timestamp << "Fuck. Not again...\n";                    */
+/*   }                                                                        */
+/*                                                                            */
+/*   ...                                                                      */
+/*                                                                            */
+/*   Log::quit();                                                             */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*   If you use Vim, you can add something like this to your .vimrc to        */
+/*   display the log file by just typing "log" while in normal mode:          */
+/*                                                                            */
+/*   nmap log :!cat Logbook/log                                               */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef LOG_H
+# define LOG_H
+
+# define LOGPATH	"Logbook/log"
+# define FOLDERPATH	"./Logbook"
+
+# define RESET		"\033[0m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+
+# include <fstream>
+# include <sys/stat.h>
+# include <ctime>
+# include <cstdio>
+# include <sstream>
+# include <sys/time.h>
+# include <string>
+
+namespace Log
+{
+	static std::ofstream	logbook;
+
+	void			init(void);
+	std::string		getTimeAndDate(void);
+	std::ofstream		&print(std::string color);
+	void			quit(void);
+}
+
+# define lout		print(GREEN)
+# define lerr		print(RED)
+# define lwar		print(YELLOW)
+# define timestamp	Log::getTimeAndDate()
+
+#endif
